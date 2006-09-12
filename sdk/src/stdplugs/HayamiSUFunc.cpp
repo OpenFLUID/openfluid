6c01ccab6b5c95590759c353cc545a2a031e566f
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2006-09-12 15:26:50 +0000

    * Created diffusive wave on SUs Hayami function plug-in

---
+++ sdk/src/stdplugs/HayamiSUFunc.cpp
@@ -0,0 +1,9 @@
+/**
+  \file
+  \brief implements ...
+
+  \author Jean-Christophe FABRE <fabrejc@ensam.inra.fr>
+*/
+
+
+#include "HayamiSUPlug.h"
---
+++ sdk/src/stdplugs/HayamiSUFunc.h
@@ -0,0 +1,17 @@
+/**
+  \file
+  \brief header of ...
+
+  \author Jean-Christophe FABRE <fabrejc@ensam.inra.fr>
+*/
+
+#ifndef __HAYAMISUFUNC_H__
+#define __HAYAMISUFUNC_H__
+
+
+
+
+#endif
+
+
+
