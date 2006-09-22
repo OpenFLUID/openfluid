7a2fe8b2a506cfd8584fc14b47d2517704a2359a
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2006-09-22 08:58:22 +0000

    * Added macros for an easy unicode management of strings (sdk-base.h)

--- app/src/sdk/sdk-base.h
+++ app/src/sdk/sdk-base.h
@@ -9,6 +9,12 @@
 #ifndef __SDKBASE_H__
 #define __SDKBASE_H__
 
+
+// =====================================================================
+// =====================================================================
+
+
+// import-export share lib defines
 #ifdef __WXMSW__
   #ifdef BUILDINGDLL
     #define DLLEXPORT __declspec(dllexport)
@@ -18,6 +24,30 @@
 #else
   #define DLLEXPORT
 #endif
+
+
+
+// =====================================================================
+// =====================================================================
+
+
+// unicode management macros
+// inspired from Code::Blocks macros http://www.codeblocks.org/
+#ifdef wxUSE_UNICODE
+    #define _U(x) wxString(x,wxConvUTF8) // char* to wxString
+    #define _UU(x,y) wxString(x,y)       // char* to wxString
+    #define _C(x) (x).mb_str(wxConvUTF8) // wxString to char*
+    #define _CC(x,y) (x).mb_str(y)       // wxString to char*
+#else
+    #define _U(x) (x)          // char* to wxString
+    #define _C(x) (x).c_str()  // wxString to char*
+#endif
+
+
+
+
+// =====================================================================
+// =====================================================================
 
 
 
