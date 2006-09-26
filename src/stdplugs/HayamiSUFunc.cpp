acaa8256741886a4cdeffb1e4c36a70da2fe1b72
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2006-09-26 10:45:59 +0000

    * Modified console output during simulation for time costs (too long to display!)

--- src/stdplugs/HayamiSUFunc.cpp
+++ src/stdplugs/HayamiSUFunc.cpp
@@ -70,6 +70,24 @@ bool HayamiSUFunc::checkConsistency()
 
 bool HayamiSUFunc::runStep(mhydasdk::base::SimulationStatus* SimStatus)
 {
+  double Value;
+
+  list<mhydasdk::core::SurfaceUnit*>::iterator iter;
+  list<mhydasdk::core::SurfaceUnit*>* SUsList = mp_CoreData->getSpatialData()->getSUsOrderedList();
+  for(iter=SUsList->begin(); iter != SUsList->end(); iter++)
+  {
+    mhydasdk::core::SurfaceUnit* SU = *iter;
+
+    if (SimStatus->getCurrentStep() > 5)
+    {
+      Value = SU->getSimulatedVars()->find(wxT("runoff"))->second->at(SimStatus->getCurrentStep()-5);
+    } else Value = 0;
+
+//    Value = (SU->getRainSource()->getTimeSerie()->getItemsCollection()->at(SimStatus->getCurrentStep())->getValue());
+    //((vector<double>*)(SU->getSimulatedVars()->find(wxT("runoff"))->second))->push_back(Value);
+    (SU->getSimulatedVars()->find(wxT("qoutput"))->second)->push_back(Value);
+
+  }
 
   // std::cout << "coucou c'est Yaya run " << mp_CoreData->getSpatialData()->getSUsCollection()->size();
   return true;
