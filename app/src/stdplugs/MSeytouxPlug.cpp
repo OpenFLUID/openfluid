fe5e11ea70f742fb84f16a62ddbeec76767ebbf0
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2006-12-01 10:07:04 +0000

    * Removed module type filter during plugins loading (according to rev 40) * Added functions type in functions signature, with one single type (SIMULATION) * Added function type filter during plugins loading, unsing type SIMULATION

--- app/src/stdplugs/MSeytouxPlug.cpp
+++ app/src/stdplugs/MSeytouxPlug.cpp
@@ -30,7 +30,7 @@ MorelSeytouxPlug::MorelSeytouxPlug()
   mp_Signature->Author = wxT("Jean-Christophe Fabre");
   mp_Signature->AuthorEmail = wxT("fabrejc@ensam.inra.fr");
   mp_Signature->ID = wxT("mseytoux");
-  mp_Signature->ModuleType = mhydasdk::base::MOD_HYDROLOGY;
+  mp_Signature->FunctionType = mhydasdk::base::SIMULATION;
   // mp_Signature->FunctionType = mhydasdk::base::FUNC_SU_PRODUCTION;
   mp_Signature->Name = wxT("Morel-Seytoux production");
   mp_Signature->Description = wxT("Production function computing infiltration and runoff at the surface of a unit using the Morel-Seytoux method, based on the Green and Ampt method;");
