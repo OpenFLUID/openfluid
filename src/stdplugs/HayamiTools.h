32feef350a0e2ab6a555e50330ccb86f4f6a8880
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2006-12-04 16:59:36 +0000

    * Added files HayamiTools.h and HayamiTools.cpp (forgotten in previous revision)

---
+++ src/stdplugs/HayamiTools.h
@@ -0,0 +1,37 @@
+/**
+  \file HayamiTools.cpp
+  \brief header of tools for Hayami propagation method
+
+  \author Jean-Christophe FABRE <fabrejc@ensam.inra.fr>
+*/
+
+
+#ifndef __HAYAMITOOLS_H__
+#define __HAYAMITOOLS_H__
+
+#include <vector>
+#include "sdk-core.h"
+
+typedef std::vector<float> t_HayamiKernel;
+
+
+/**
+  Computes the Hayami Kernel
+  \param[in] Celerity Wave propagation (m/s)
+  \param[in] Sigma Wave diffusivity (m2/s)
+  \param[in] Length Transfer Length (m)
+  \param[in] MaxSteps Maximum steps for the kernel length
+  \param[in] TimeStep Time step duration (s)
+  \return a vector of double containing the kernel      
+*/
+
+ t_HayamiKernel ComputeHayamiKernel(float Celerity, float Sigma, float Length, int MaxSteps, int TimeStep);
+
+
+/**
+  Propagates the wave using the Hayami kernel
+*/
+float DoHayamiPropagation(t_HayamiKernel Kernel, int CurrentStep, mhydasdk::core::VectorOfDouble* QInput, int MaxSteps, int TimeStep);
+
+
+#endif // __HAYAMITOOLS_H__
