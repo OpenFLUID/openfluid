406dcf46e49e8a34ac3672def1f09b5c108c70ab
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2007-02-23 16:35:18 +0000

    * Modified rain event repository for pre-processing of input rain before simulation. however, there is currently no processing of the input rain * Added method for rain data access from simulation functions (sdk,Function.h), for a later replacement of macro

--- app/src/sdk/core/CoreRepository.cpp
+++ app/src/sdk/core/CoreRepository.cpp
@@ -7,7 +7,7 @@
 
 
 #include "CoreRepository.h"
-
+#include <iostream>
 
 namespace mhydasdk { namespace core {
 
@@ -16,6 +16,8 @@ CoreRepository::CoreRepository()
 {
   mp_SpatialData = new SpatialRepository();
   mp_RainEvent = new RainEvent();
+  
+//  mp_PcssRainEvent = NULL;
 
 }
 
@@ -27,7 +29,14 @@ CoreRepository::~CoreRepository()
 
 }
 
-
-
+// =====================================================================
+// =====================================================================
+/*
+bool CoreRepository::ProcessRainEvent(int TimeStep)
+{
+  mp_PcssRainEvent = new ProcessedRainEvent(mp_RainEvent,TimeStep);   
+  return true;
+}
+*/
 
 } } // namespace mhydasdk::core
