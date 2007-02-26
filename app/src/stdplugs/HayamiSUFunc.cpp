f94e66bdf82a2a92cec9790e7457ced14c7a470b
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2007-02-26 16:42:59 +0000

    * Deactivated (commented) macros for core data acess (Function.h) * Modified simulation functions considering the macro deactivation

--- app/src/stdplugs/HayamiSUFunc.cpp
+++ app/src/stdplugs/HayamiSUFunc.cpp
@@ -67,6 +67,7 @@ bool HayamiSUFunction::initializeRun(mhydasdk::base::SimulationInfo* SimInfo)
   mhydasdk::core::SurfaceUnit* SU;
   float Cel, Sigma;
   int ID;
+  float TmpValue;
 
   DECLARE_SU_ORDERED_LOOP;
  
@@ -78,15 +79,20 @@ bool HayamiSUFunction::initializeRun(mhydasdk::base::SimulationInfo* SimInfo)
     m_CurrentInputSum[ID] = 0;
   
     m_MeanSlope = m_MeanSlope + SU->getUsrSlope();
-    m_MeanManning = m_MeanManning + SU->getProperties()->find(wxT("nmanning"))->second;    
+    MHYDAS_GetDistributedProperty(SU,wxT("nmanning"),&TmpValue);
+    m_MeanManning = m_MeanManning + TmpValue;
+    //m_MeanManning = m_MeanManning + SU->getProperties()->find(wxT("nmanning"))->second;    
   END_LOOP
 
   m_MeanSlope = m_MeanSlope / mp_CoreData->getSpatialData()->getSUsCollection()->size();
   m_MeanManning = m_MeanManning / mp_CoreData->getSpatialData()->getSUsCollection()->size(); 
 
   BEGIN_SU_ORDERED_LOOP(SU)
-    Cel = m_MeanCelerity * (m_MeanManning / SU->getProperties()->find(wxT("nmanning"))->second) * (sqrt((SU->getUsrSlope() / m_MeanSlope)));
-    Sigma = m_MeanSigma * (SU->getProperties()->find(wxT("nmanning"))->second / m_MeanManning) * (m_MeanSlope / SU->getUsrSlope());      
+    MHYDAS_GetDistributedProperty(SU,wxT("nmanning"),&TmpValue);
+    // Cel = m_MeanCelerity * (m_MeanManning / SU->getProperties()->find(wxT("nmanning"))->second) * (sqrt((SU->getUsrSlope() / m_MeanSlope)));
+    // Sigma = m_MeanSigma * (SU->getProperties()->find(wxT("nmanning"))->second / m_MeanManning) * (m_MeanSlope / SU->getUsrSlope());      
+    Cel = m_MeanCelerity * (m_MeanManning / TmpValue) * (sqrt((SU->getUsrSlope() / m_MeanSlope)));
+    Sigma = m_MeanSigma * (TmpValue / m_MeanManning) * (m_MeanSlope / SU->getUsrSlope());    
     m_SUKernel[SU->getID()] = ComputeHayamiKernel(Cel, Sigma,SU->getDownstreamDistance(),m_MaxSteps,SimInfo->getTimeStep());    
   END_LOOP
 
@@ -118,6 +124,7 @@ bool HayamiSUFunction::runStep(mhydasdk::base::SimulationStatus* SimStatus)
   int TimeStep;
   float QOutput;
   float QInput;
+  float TmpValue;
 
   
   mhydasdk::core::SurfaceUnit* SU;
@@ -128,8 +135,10 @@ bool HayamiSUFunction::runStep(mhydasdk::base::SimulationStatus* SimStatus)
   DECLARE_SU_ORDERED_LOOP;
   BEGIN_SU_ORDERED_LOOP(SU)
 
-    ID = SU->getID();   
-    QInput = GET_SIMVAR_VALUE(SU,"runoff",CurrentStep) * SU->getUsrArea() / TimeStep;
+    ID = SU->getID();
+    MHYDAS_GetDistributedVarValue(SU,wxT("runoff"),CurrentStep,&TmpValue);
+    //QInput = GET_SIMVAR_VALUE(SU,"runoff",CurrentStep) * SU->getUsrArea() / TimeStep;
+    QInput = TmpValue * SU->getUsrArea() / TimeStep;
     m_CurrentInputSum[ID] = m_CurrentInputSum[ID] + QInput;
     m_Input[ID]->push_back(QInput);
     
@@ -139,7 +148,8 @@ bool HayamiSUFunction::runStep(mhydasdk::base::SimulationStatus* SimStatus)
       QOutput = DoHayamiPropagation(m_SUKernel[ID], CurrentStep, m_Input[ID], m_MaxSteps, TimeStep);
     }  
         
-    APPEND_SIMVAR_VALUE(SU,"qoutput",QOutput);
+    //APPEND_SIMVAR_VALUE(SU,"qoutput",QOutput);
+    MHYDAS_AppendDistributedVarValue(SU,wxT("qoutput"),QOutput);
 
   END_LOOP
   
