87152c4811102053afd2b03ecd14939fde6fa1a7
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2008-07-23 07:02:40 +0000

    * Modified functions, according to the ID-maps refactoring (IDVectorValuePtrMap in place of IDVectorValueMap)

--- src/functions/mhydas/water.surf.transfer-rs.hayami/HayamiRSFunc.h
+++ src/functions/mhydas/water.surf.transfer-rs.hayami/HayamiRSFunc.h
@@ -45,11 +45,11 @@ class HayamiRSFunction : public openfluid::base::PluggableFunction
 
     bool m_UseUpSUOutput;
 
-    openfluid::core::IDVectorValueMap m_Input;
+    openfluid::core::IDVectorValuePtrMap m_Input;
 
     openfluid::core::IDScalarValueMap m_CurrentInputSum;
 
-    openfluid::core::IDVectorValueMap m_HeightDischarge;
+    openfluid::core::IDVectorValuePtrMap m_HeightDischarge;
 
     bool computeWaterHeightFromDischarge(openfluid::core::UnitID ID, openfluid::core::ScalarValue Discharge, openfluid::core::ScalarValue *Height);
 
