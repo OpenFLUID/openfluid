c7215cddd9d7cb4b2c68e7bf147610c8ea97950a
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2009-08-20 14:14:43 +0000

    * Added technical comments

--- include/tools/ColTextParser.h
+++ include/tools/ColTextParser.h
@@ -55,35 +55,76 @@ class ColumnTextParser
     */
     ~ColumnTextParser();
 
+
+    /**
+      Loads a column text file
+      \param[in] Filename the full path of the file to load
+      \return true if everything went fine
+    */
     bool loadFromFile(std::string Filename);
 
+    /**
+      Parses a one-line delimiter-separated string to set contents
+      ex: "147.2 18 15 25 36 51.3 25.1 15", for 4 columns
+      \param[in] Content the string to parse
+      \param[in] ColumnsNbr the number of columns (for splitting the sting into lines)
+      \return true if everything went fine
+    */
     bool setFromString(std::string Contents, unsigned int ColumnsNbr);
 
     /**
       Returns the value at a specified row-column, as a string
+      \param[in] Line the line number of the value (first line is 0)
+      \param[in] Column the column number of the value (first column is 0)
+      \return the requested value
     */
     std::string getValue(unsigned int Line, unsigned int Column);
 
+    /**
+      Gets the value at a specified row-column, as a string
+      \param[in] Line the line number of the value (first line is 0)
+      \param[in] Column the column number of the value (first column is 0)
+      \param[out] Value the requested value if exists
+      \return true if the value has been found, false otherwise
+    */
     bool getStringValue(unsigned int Line, unsigned int Column, std::string *Value);
 
     /**
-      Returns the value at a specified row-column, as an int
+      Gets the value at a specified row-column, as a long int
+      \param[in] Line the line number of the value (first line is 0)
+      \param[in] Column the column number of the value (first column is 0)
+      \param[out] Value the requested value if exists
+      \return true if the value has been found, false otherwise
     */
     bool getLongValue(unsigned int Line, unsigned int Column, long* Value);
 
 
     /**
-      Returns the value at a specified row-column, as a double
+      Gets the value at a specified row-column, as a double precision float
+      \param[in] Line the line number of the value (first line is 0)
+      \param[in] Column the column number of the value (first column is 0)
+      \param[out] Value the requested value if exists
+      \return true if the value has been found, false otherwise
     */
     bool getDoubleValue(unsigned int Line, unsigned int Column, double* Value);
 
     /**
-      Returns the values of a line as astring list
+      Returns the values at a specified line
+      \param[in] Line the line number of the value (first line is 0)
+      \return the requested values
     */
     std::vector<std::string> getValues(unsigned int Line);
 
+    /**
+      Returns the number of lines
+      \return the number of lines
+    */
     int getLinesCount() const { return m_LinesCount;};
 
+    /**
+      Returns the number of columns
+      \return the number of columns
+    */
     int getColsCount() const { return m_ColsCount;};
 
     void coutContents();
