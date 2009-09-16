faf8a885364e6943b9d0c993029d717abafc8349
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2009-09-16 19:42:42 +0000

    * Added unit tests for various ofelib and kernel classes and subprograms

---
+++ tests/unittesting/kernel/RuntimeEnv_TEST.cpp
@@ -0,0 +1,41 @@
+/***copyright>=========================================================
+
+  OpenFLUID - Software environment for modelling Fluxes In Landscapes
+
+  This file is a part of OpenFLUID software
+  http://www.umr-lisah.fr/openfluid
+  Copyright (c) INRA
+  See the file "copyright" for the full license governing this code.
+
+=========================================================<copyright***/
+
+
+/**
+  \file RuntimeEnv_TEST.cpp
+  \brief Implements ...
+
+  \author Jean-Christophe FABRE <fabrejc@supagro.inra.fr>
+ */
+
+
+#define BOOST_TEST_MAIN
+#define BOOST_AUTO_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE unittest_runtimeenv
+#include <boost/test/unit_test.hpp>
+#include <boost/test/auto_unit_test.hpp>
+#include "openfluid-core.h"
+
+
+// =====================================================================
+// =====================================================================
+
+
+BOOST_AUTO_TEST_CASE(check_construction)
+{
+  BOOST_FAIL("under construction");
+}
+
+// =====================================================================
+// =====================================================================
+
