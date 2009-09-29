9a64c65fbd6f310a29c99de4bc4aa4dd24091dca
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2009-09-29 10:22:49 +0000

    * Added MemoryMonitor unit tests

--- tests/unittesting/ofelib/MemMonitor_TEST.cpp
+++ tests/unittesting/ofelib/MemMonitor_TEST.cpp
@@ -33,9 +33,53 @@
 
 BOOST_AUTO_TEST_CASE(check_construction)
 {
-  BOOST_FAIL("under construction");
+  openfluid::core::MemoryMonitor *MM;
+
+  MM = openfluid::core::MemoryMonitor::getInstance();
+
+  BOOST_REQUIRE_EQUAL(MM->getPacket(),0);
+  BOOST_REQUIRE_EQUAL(MM->getKeep(),0);
+  BOOST_REQUIRE_EQUAL(MM->getLastMemoryRelease(),-1);
 }
 
 // =====================================================================
 // =====================================================================
 
+
+void check_operation_withparams(unsigned int Packet, unsigned int Keep, unsigned int StepsCount)
+{
+  openfluid::core::MemoryMonitor *MM;
+
+  MM = openfluid::core::MemoryMonitor::getInstance();
+  MM->setPacketAndKeep(Packet,Keep);
+  MM->setLastMemoryRelease(Keep-1);
+
+  BOOST_REQUIRE_EQUAL(MM->getPacket(),Packet);
+  BOOST_REQUIRE_EQUAL(MM->getKeep(),Keep);
+  BOOST_REQUIRE_EQUAL(MM->getLastMemoryRelease(),-1);
+
+  for (unsigned int i = 0; i < StepsCount; i++)
+  {
+    if (MM->isMemReleaseStep(i))
+    {
+      BOOST_REQUIRE_EQUAL(i-MM->getLastMemoryRelease(),MM->getPacket()+MM->getKeep());
+      BOOST_REQUIRE_NE(MM->getLastMemoryRelease(),i-MM->getKeep());
+      MM->setLastMemoryRelease(i);
+      BOOST_REQUIRE_EQUAL(MM->getLastMemoryRelease(),i-MM->getKeep());
+    }
+    else
+    {
+      BOOST_REQUIRE_NE(i-MM->getLastMemoryRelease(),MM->getPacket()+MM->getKeep());
+    }
+  }
+}
+
+BOOST_AUTO_TEST_CASE(check_operations)
+{
+  check_operation_withparams(500,10,10000);
+  check_operation_withparams(1,1,10000);
+  check_operation_withparams(5000,100,100);
+}
+
+// =====================================================================
+// =====================================================================
