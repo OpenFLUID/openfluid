3c189d8426ec31d13b028675645922d01643efa7
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2009-09-29 12:04:19 +0000

    * Added FunctionEnvironment unit tests

--- tests/unittesting/ofelib/FunctionEnv_TEST.cpp
+++ tests/unittesting/ofelib/FunctionEnv_TEST.cpp
@@ -24,6 +24,7 @@
 #include <boost/test/unit_test.hpp>
 #include <boost/test/auto_unit_test.hpp>
 #include "openfluid-core.h"
+#include "openfluid-base.h"
 
 
 // =====================================================================
@@ -32,7 +33,56 @@
 
 BOOST_AUTO_TEST_CASE(check_construction)
 {
-  BOOST_FAIL("under construction");
+  openfluid::base::FunctionEnvironment FuncEnv;
+  bool BoolValue;
+  std::string StrValue;
+
+  BOOST_REQUIRE_EQUAL(FuncEnv.getValue("fakebool",&BoolValue),false);
+  BOOST_REQUIRE_EQUAL(FuncEnv.getValue("fakestring",&StrValue),false);
+
+}
+
+// =====================================================================
+// =====================================================================
+
+BOOST_AUTO_TEST_CASE(check_operations)
+{
+  openfluid::base::FunctionEnvironment FuncEnv;
+  bool BoolValue;
+  std::string StrValue;
+
+
+
+  BOOST_REQUIRE_EQUAL(FuncEnv.setValue("bool1",true),true);
+  BOOST_REQUIRE_EQUAL(FuncEnv.getValue("bool1",&BoolValue),true);
+  BOOST_REQUIRE_EQUAL(BoolValue,true);
+
+  BOOST_REQUIRE_EQUAL(FuncEnv.setValue("bool1",false),true);
+  BOOST_REQUIRE_EQUAL(FuncEnv.getValue("bool1",&BoolValue),true);
+  BOOST_REQUIRE_EQUAL(BoolValue,false);
+
+  BOOST_REQUIRE_EQUAL(FuncEnv.setValue("bool2",false),true);
+  BOOST_REQUIRE_EQUAL(FuncEnv.getValue("bool2",&BoolValue),true);
+  BOOST_REQUIRE_EQUAL(BoolValue,false);
+
+  BOOST_REQUIRE_EQUAL(FuncEnv.getValue("fakebool",&BoolValue),false);
+
+
+
+  BOOST_REQUIRE_EQUAL(FuncEnv.setValue("str1",std::string("first string")),true);
+  BOOST_REQUIRE_EQUAL(FuncEnv.getValue("str1",&StrValue),true);
+  BOOST_REQUIRE_EQUAL(StrValue,"first string");
+
+  BOOST_REQUIRE_EQUAL(FuncEnv.setValue("str1",std::string("string one")),true);
+  BOOST_REQUIRE_EQUAL(FuncEnv.getValue("str1",&StrValue),true);
+  BOOST_REQUIRE_EQUAL(StrValue,"string one");
+
+  BOOST_REQUIRE_EQUAL(FuncEnv.setValue("str2",std::string("string 2")),true);
+  BOOST_REQUIRE_EQUAL(FuncEnv.getValue("str2",&StrValue),true);
+  BOOST_REQUIRE_EQUAL(StrValue,"string 2");
+
+  BOOST_REQUIRE_EQUAL(FuncEnv.getValue("fakestring",&StrValue),false);
+
 }
 
 // =====================================================================
