f706e96f91902f55d9092b7aab05fd0d7c33f6ca
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2009-10-05 14:26:06 +0000

    * Modified Func2Doc buddy, added latex file generation (signature extraction sill incomplete), PDF and HTML compilation * Modified tests for Func2Doc buddy

--- src/kernel/buddies/Func2DocBuddy.cpp
+++ src/kernel/buddies/Func2DocBuddy.cpp
@@ -31,18 +31,25 @@
 
 Func2DocBuddy::Func2DocBuddy() : OpenFLUIDBuddy()
 {
-  m_BeginSignatureTag = "BEGIN_SIGNATURE_HOOK";
-  m_EndSignatureTag = "END_SIGNATURE_HOOK";
-  m_BeginFunc2DocTag = "<func2doc>";
-  m_EndFunc2DocTag = "</func2doc>";
-
-
   m_RequiredOptionsHelp["outputdir"] = "path for generated files";
   m_RequiredOptionsHelp["inputcpp"] = "path for cpp file to parse";
 
   m_OtherOptionsHelp["html"] = "set to 1 in order to generate documentation as HTML files";
   m_OtherOptionsHelp["pdf"] = "set to 1 in order to generate documentation as PDF file";
   m_OtherOptionsHelp["tplfile"] = "path to template file";
+
+
+  m_Title = "Unknown Title";
+  m_FuncID = "unknown.id";
+  m_FuncName = "Unknown Function Name";
+  m_FuncAuthor = "Unknown Function Author";
+  m_FuncEmail = "Unknown Function Author Email";
+  m_FuncDomain = "Unknown Function Domain";
+
+  m_BeginSignatureTag = "BEGIN_SIGNATURE_HOOK";
+  m_EndSignatureTag = "END_SIGNATURE_HOOK";
+  m_BeginFunc2DocTag = "<func2doc>";
+  m_EndFunc2DocTag = "</func2doc>";
 
 }
 
@@ -102,8 +109,6 @@ void Func2DocBuddy::extractLatexDocFromCPP()
   CPPFile.close();
 
 
-
-
   std::string::size_type Index = CPPFileContent.find(m_BeginFunc2DocTag);
 
   if (Index != std::string::npos)
@@ -150,7 +155,9 @@ void Func2DocBuddy::cpreprocessCPP()
 void Func2DocBuddy::processSignature()
 {
   std::cout << "** Processing function signature...";
+
   std::cout << " done" << std::endl;
+
 }
 
 
@@ -162,17 +169,44 @@ void Func2DocBuddy::generateLatex()
 {
   std::cout << "** Generating latex file...";
 
-  // extraction of func2doc content
+  // loading template
 
-  // C preprocessing of original file using gcc
+  std::ifstream TPLFile(m_TplFilePath.string().c_str());
 
-  // extraction of signature lines from C preprocessed file
+  // check if file exists and if it is "openable"
+  if (!TPLFile)
+    throw openfluid::base::OFException("kernel","Func2DocBuddy::generateLatex()","Could not open template file");
 
-  // processing of signature lines
+  std::string StrLine  = "";
+  m_LatexOutFile = "";
 
-  // build of latex file content
+  // parse and loads file contents
+  while(std::getline(TPLFile,StrLine))
+  {
+    m_LatexOutFile = m_LatexOutFile + StrLine + "\n";
+  }
+
+  TPLFile.close();
+
+  // replacing values
+
+  boost::algorithm::replace_all(m_LatexOutFile,"#title#",m_Title);
+  boost::algorithm::replace_all(m_LatexOutFile,"#author#",m_FuncAuthor);
+  boost::algorithm::replace_all(m_LatexOutFile,"#newcommands#",m_NewCommands);
+  boost::algorithm::replace_all(m_LatexOutFile,"#name#",m_FuncName);
+  boost::algorithm::replace_all(m_LatexOutFile,"#domain#",m_FuncDomain);
+  boost::algorithm::replace_all(m_LatexOutFile,"#description#",m_FuncDescription);
+  boost::algorithm::replace_all(m_LatexOutFile,"#handleddata#",m_FuncData);
+  boost::algorithm::replace_all(m_LatexOutFile,"#funcdoc#",m_ExtractedLatexDoc);
 
   // save of latex file content
+
+  m_OutputLatexFilePath = boost::filesystem::path(m_OutputDirPath.string()+"/"+m_FuncID+".tex");
+
+  std::ofstream OutputFile(m_OutputLatexFilePath.string().c_str(),std::ios::out);
+  OutputFile << m_LatexOutFile;
+  OutputFile.close();
+
 
   std::cout << " done" << std::endl;
 }
@@ -186,13 +220,25 @@ void Func2DocBuddy::buildPDF()
 
   std::cout << "** Building PDF...";
 
+  std::string PDFCommandToRun = m_PDFLatexPath.string() + " -shell-escape -interaction=nonstopmode -output-directory="+m_OutputDirPath.string()+" "+ m_OutputLatexFilePath.string() + " > /dev/null";
+  std::string BibCommandToRun = m_BibtexPath.string() + " " + boost::filesystem::path(m_OutputDirPath.string()+"/"+m_FuncID).string() + " > /dev/null";
+
   std::cout << " first pass...";
+
+  std::cout << PDFCommandToRun << std::endl;
+  system(PDFCommandToRun.c_str());
 
   std::cout << " bibliography and references...";
 
+  system(BibCommandToRun.c_str());
+
   std::cout << " second pass...";
 
+  system(PDFCommandToRun.c_str());
+
   std::cout << " third pass...";
+
+  system(PDFCommandToRun.c_str());
 
   std::cout << " done" << std::endl;
 
@@ -207,6 +253,10 @@ void Func2DocBuddy::buildHTML()
 {
   std::cout << "** Building HTML...";
 
+  std::string CommandToRun = m_Latex2HTMLPath.string() + " -dir="+m_OutputDirPath.string()+" "+ m_OutputLatexFilePath.string();
+
+  system(CommandToRun.c_str());
+
   std::cout << " done" << std::endl;
 }
 
@@ -218,9 +268,9 @@ void Func2DocBuddy::buildHTML()
 
 bool Func2DocBuddy::run()
 {
-#ifdef __unix__
+#if defined __unix__ || defined __APPLE__
 
-  setOptionIfNotSet("tplfile",FUNCDOC_TPLFILE_PATH + "/" + FUNCDOC_TPLFILE_PATH);
+  setOptionIfNotSet("tplfile",FUNC2DOC_TPLFILE_PATH + "/" + FUNC2DOC_TPLFILE_PATH);
   setOptionIfNotSet("outputdir",boost::filesystem::current_path().string());
   setOptionIfNotSet("pdf","0");
   setOptionIfNotSet("html","0");
@@ -231,7 +281,44 @@ bool Func2DocBuddy::run()
   std::cout << "Generate PDF: " << getYesNoFromOneZero(m_Options["pdf"]) << std::endl;
   std::cout << "Generate HTML: " << getYesNoFromOneZero(m_Options["html"]) << std::endl;
 
-  m_CPreProcessorPath = boost::filesystem::path("/usr/bin/gcc");
+
+  std::vector<std::string> GCCPaths = GetFileLocationsUsingPATHEnvVar("gcc");
+
+  if (GCCPaths.size() > 0)
+  {
+    m_CPreProcessorPath = boost::filesystem::path(GCCPaths[0]);
+    std::cout << "Using C preprocessor: " << m_CPreProcessorPath.string() << std::endl;
+  }
+  else throw openfluid::base::OFException("kernel","Func2DocBuddy::run()","C preprocessor (gcc) not found");
+
+
+  if (m_Options["pdf"] == "1")
+  {
+    std::vector<std::string> PDFLatexPaths = GetFileLocationsUsingPATHEnvVar("pdflatex");
+    std::vector<std::string> BibtexPaths = GetFileLocationsUsingPATHEnvVar("bibtex");
+
+    if (PDFLatexPaths.size() > 0 && BibtexPaths.size() > 0)
+      {
+        m_PDFLatexPath = boost::filesystem::path(PDFLatexPaths[0]);
+        std::cout << "Using PDFLatex compiler: " << m_PDFLatexPath.string() << std::endl;
+        m_BibtexPath = boost::filesystem::path(BibtexPaths[0]);
+        std::cout << "Using Bibtex compiler: " << m_BibtexPath.string() << std::endl;
+      }
+      else std::cout << "!! PDFLatex compiler and/or Bibtex compiler not found. Skipped. " << std::endl;
+  }
+
+  if (m_Options["html"] == "1")
+  {
+    std::vector<std::string> Latex2HTMLPaths = GetFileLocationsUsingPATHEnvVar("latex2html");
+
+    if (Latex2HTMLPaths.size() > 0)
+      {
+        m_Latex2HTMLPath = boost::filesystem::path(Latex2HTMLPaths[0]);
+        std::cout << "Using Latex2HTML converter: " << m_Latex2HTMLPath.string() << std::endl;
+      }
+      else std::cout << "!! Latex2HTML converter not found. Skipped. " << std::endl;
+  }
+
 
   m_InputFilePath = boost::filesystem::path(m_Options["inputcpp"]);
   m_OutputDirPath = boost::filesystem::path(m_Options["outputdir"]);
@@ -239,7 +326,12 @@ bool Func2DocBuddy::run()
   if (!boost::filesystem::exists(m_InputFilePath))
     throw openfluid::base::OFException("kernel","Func2DocBuddy::run()","Input file does not exist");
 
+  m_TplFilePath = boost::filesystem::path(m_Options["tplfile"]);
+  if (!boost::filesystem::exists(m_TplFilePath))
+    throw openfluid::base::OFException("kernel","Func2DocBuddy::run()","Template file does not exist");
+
   if (!boost::filesystem::is_directory(m_OutputDirPath)) boost::filesystem::create_directories(m_OutputDirPath);
+
 
   m_InputDirPath = m_InputFilePath.branch_path();
 
@@ -258,14 +350,15 @@ bool Func2DocBuddy::run()
 
   if (m_Options["html"] == "1") buildHTML();
 
+  throw openfluid::base::OFException("under construction");
 
 #else
-  std::cout << "func2doc buddy only runs on unix systems." << std::endl;
+  std::cout << "func2doc buddy only runs on Unix/Linux and MacOSX systems." << std::endl;
   return true;
 #endif
 
-  throw openfluid::base::OFException("under construction");
 
   return true;
 }
+
 
--- src/kernel/buddies/Func2DocBuddy.h
+++ src/kernel/buddies/Func2DocBuddy.h
@@ -36,31 +36,40 @@ class Func2DocBuddy : public OpenFLUIDBuddy
     boost::filesystem::path m_InputDirPath;
     boost::filesystem::path m_OutputDirPath;
     boost::filesystem::path m_InputFilePath;
+    boost::filesystem::path m_OutputLatexFilePath;
+    boost::filesystem::path m_TplFilePath;
     boost::filesystem::path m_CProcessedFilePath;
+    boost::filesystem::path m_PDFLatexPath;
+    boost::filesystem::path m_BibtexPath;
+    boost::filesystem::path m_Latex2HTMLPath;
 
     boost::filesystem::path m_CPreProcessorPath;
 
     std::string m_ExtractedLatexDoc;
 
     std::string m_Title;
+    std::string m_NewCommands;
+    std::string m_FuncID;
     std::string m_FuncName;
     std::string m_FuncAuthor;
+    std::string m_FuncEmail;
     std::string m_FuncDomain;
+    std::string m_FuncDescription;
+    std::string m_FuncData;
 
     std::string m_BeginSignatureTag;
     std::string m_EndSignatureTag;
     std::string m_BeginFunc2DocTag;
     std::string m_EndFunc2DocTag;
 
-    std::string m_FuncDoc;
 
-    SignatureData_t m_InVars;
-    SignatureData_t m_OutVars;
-    SignatureData_t m_ModVars;
-    SignatureData_t m_InData;
-    SignatureData_t m_ParamsData;
-    SignatureData_t m_Events;
-    SignatureData_t m_ExtraFiles;
+//    SignatureData_t m_InVars;
+//    SignatureData_t m_OutVars;
+//    SignatureData_t m_ModVars;
+//    SignatureData_t m_InData;
+//    SignatureData_t m_ParamsData;
+//    SignatureData_t m_Events;
+//    SignatureData_t m_ExtraFiles;
 
 
     void copyDocDirectory();
