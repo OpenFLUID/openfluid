479ff40dda9070931eb1abeb845f882a6e80d1c5
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2009-10-08 07:19:24 +0000

    * Added test macro for classes from the kernel * Added SwissTools and RuntimeEnvironment tests

--- tests/unittesting/kernel/RuntimeEnv_TEST.cpp
+++ tests/unittesting/kernel/RuntimeEnv_TEST.cpp
@@ -24,7 +24,9 @@
 #define BOOST_TEST_MODULE unittest_runtimeenv
 #include <boost/test/unit_test.hpp>
 #include <boost/test/auto_unit_test.hpp>
-#include "openfluid-core.h"
+#include "RuntimeEnv.h"
+#include "openfluid-base.h"
+#include <wx/app.h>
 
 
 // =====================================================================
@@ -33,9 +35,132 @@
 
 BOOST_AUTO_TEST_CASE(check_construction)
 {
-  BOOST_FAIL("under construction");
+  if (!wxInitialize())
+  {
+    BOOST_FAIL("Error initializing wx libs");
+  }
+
+  RuntimeEnvironment RunEnv;
+
+  BOOST_REQUIRE_NE(RunEnv.getInputDir(),"");
+  BOOST_REQUIRE_NE(RunEnv.getOutputDir(),"");
+  BOOST_REQUIRE_NE(RunEnv.getTempDir(),"");
+  BOOST_REQUIRE_GT(RunEnv.getPluginsPaths().size(),0);
+  BOOST_REQUIRE_EQUAL(RunEnv.isClearOutputDir(),false);
+  BOOST_REQUIRE_EQUAL(RunEnv.isVerboseRun(),false);
+  BOOST_REQUIRE_EQUAL(RunEnv.isQuietRun(),false);
+  BOOST_REQUIRE_EQUAL(RunEnv.isWriteResults(),true);
+  BOOST_REQUIRE_EQUAL(RunEnv.isWriteSimReport(),true);
+  BOOST_REQUIRE_EQUAL(RunEnv.isProgressiveOutput(),false);
+  BOOST_REQUIRE_EQUAL(RunEnv.isCheckVarNames(),true);
+  BOOST_REQUIRE_NE(RunEnv.getSimulationID(),"");
+
+  wxUninitialize();
 }
 
 // =====================================================================
 // =====================================================================
 
+BOOST_AUTO_TEST_CASE(check_operations)
+{
+  if (!wxInitialize())
+  {
+    BOOST_FAIL("Error initializing wx libs");
+  }
+
+  RuntimeEnvironment RunEnv;
+
+  RunEnv.setInputDir("/foo/bar");
+  RunEnv.setOutputDir("/bar/foo/bar");
+  RunEnv.setOutputDir("/bar/foo/bar");
+  RunEnv.addExtraPluginsPaths("/bar/foo/foo/bar");
+  RunEnv.addExtraPluginsPaths("/bar/foo/foo/bar/bar/bar");
+  BOOST_REQUIRE_EQUAL(RunEnv.getInputDir(),"/foo/bar");
+  BOOST_REQUIRE_EQUAL(RunEnv.getOutputDir(),"/bar/foo/bar");
+  BOOST_REQUIRE_NE(RunEnv.getTempDir(),"");
+  BOOST_REQUIRE_GT(RunEnv.getPluginsPaths().size(),2);
+
+
+  RunEnv.setClearOutputDir(true);
+  BOOST_REQUIRE_EQUAL(RunEnv.isClearOutputDir(),true);
+
+  RunEnv.setVerboseRun(true);
+  BOOST_REQUIRE_EQUAL(RunEnv.isVerboseRun(),true);
+  BOOST_REQUIRE_EQUAL(RunEnv.isQuietRun(),false);
+
+  RunEnv.setVerboseRun(false);
+  BOOST_REQUIRE_EQUAL(RunEnv.isVerboseRun(),false);
+  BOOST_REQUIRE_EQUAL(RunEnv.isQuietRun(),false);
+
+  RunEnv.setVerboseRun(true);
+  RunEnv.setQuietRun(true);
+  BOOST_REQUIRE_EQUAL(RunEnv.isVerboseRun(),false);
+  BOOST_REQUIRE_EQUAL(RunEnv.isQuietRun(),true);
+
+  RunEnv.setVerboseRun(false);
+  RunEnv.setQuietRun(false);
+  BOOST_REQUIRE_EQUAL(RunEnv.isVerboseRun(),false);
+  BOOST_REQUIRE_EQUAL(RunEnv.isQuietRun(),false);
+
+  RunEnv.setWriteResults(false);
+  BOOST_REQUIRE_EQUAL(RunEnv.isWriteResults(),false);
+
+  RunEnv.setWriteSimReport(false);
+  BOOST_REQUIRE_EQUAL(RunEnv.isWriteSimReport(),false);
+
+  RunEnv.setCheckVarNames(false);
+  BOOST_REQUIRE_EQUAL(RunEnv.isCheckVarNames(),false);
+
+  RunEnv.setProgressiveOutputKeep(23);
+  RunEnv.setProgressiveOutputPacket(2345);
+  BOOST_REQUIRE_EQUAL(RunEnv.isProgressiveOutput(),true);
+  BOOST_REQUIRE_EQUAL(RunEnv.getProgressiveOutputPacket(),2345);
+  BOOST_REQUIRE_EQUAL(RunEnv.getProgressiveOutputKeep(),23);
+
+  RunEnv.setSimulationID("THESIMID-2");
+  BOOST_REQUIRE_EQUAL(RunEnv.getSimulationID(),"THESIMID-2");
+
+
+
+
+  openfluid::base::FunctionEnvironment* FuncEnv;
+  bool BoolValue;
+  std::string StrValue;
+
+  FuncEnv = RunEnv.getFunctionEnvironment();
+  BOOST_REQUIRE(FuncEnv != NULL);
+
+  BOOST_REQUIRE_EQUAL(FuncEnv->getValue("wrong.fake",&StrValue),false);
+  BOOST_REQUIRE_EQUAL(FuncEnv->getValue("fake.wrong",&BoolValue),false);
+
+  BOOST_REQUIRE_EQUAL(FuncEnv->getValue("dir.input",&StrValue),true);
+  BOOST_REQUIRE_EQUAL(StrValue,RunEnv.getInputDir());
+
+  BOOST_REQUIRE_EQUAL(FuncEnv->getValue("dir.output",&StrValue),true);
+  BOOST_REQUIRE_EQUAL(StrValue,RunEnv.getOutputDir());
+
+  BOOST_REQUIRE_EQUAL(FuncEnv->getValue("dir.temp",&StrValue),true);
+  BOOST_REQUIRE_EQUAL(StrValue,RunEnv.getTempDir());
+
+  BOOST_REQUIRE_EQUAL(FuncEnv->getValue("mode.clearoutputdir",&BoolValue),true);
+  BOOST_REQUIRE_EQUAL(BoolValue,RunEnv.isClearOutputDir());
+
+  BOOST_REQUIRE_EQUAL(FuncEnv->getValue("mode.quiet",&BoolValue),true);
+  BOOST_REQUIRE_EQUAL(BoolValue,RunEnv.isQuietRun());
+
+  BOOST_REQUIRE_EQUAL(FuncEnv->getValue("mode.verbose",&BoolValue),true);
+  BOOST_REQUIRE_EQUAL(BoolValue,RunEnv.isVerboseRun());
+
+  BOOST_REQUIRE_EQUAL(FuncEnv->getValue("mode.saveresults",&BoolValue),true);
+  BOOST_REQUIRE_EQUAL(BoolValue,RunEnv.isWriteResults());
+
+  BOOST_REQUIRE_EQUAL(FuncEnv->getValue("mode.writereport",&BoolValue),true);
+  BOOST_REQUIRE_EQUAL(BoolValue,RunEnv.isWriteSimReport());
+
+
+
+  wxUninitialize();
+}
+
+// =====================================================================
+// =====================================================================
--- tests/unittesting/ofelib/SwissTools_TEST.cpp
+++ tests/unittesting/ofelib/SwissTools_TEST.cpp
@@ -24,16 +24,61 @@
 #define BOOST_TEST_MODULE unittest_swisstools
 #include <boost/test/unit_test.hpp>
 #include <boost/test/auto_unit_test.hpp>
-#include "openfluid-core.h"
+#include "openfluid-tools.h"
 
 
 // =====================================================================
 // =====================================================================
 
 
-BOOST_AUTO_TEST_CASE(check_construction)
+BOOST_AUTO_TEST_CASE(check_operations)
 {
-  BOOST_FAIL("under construction");
+  std::string Str;
+  bool BoolValue;
+  double DoubleValue;
+  std::vector<std::string> StrArray;
+
+  Str = "0.01";
+  BOOST_REQUIRE_EQUAL(openfluid::tools::ConvertString(Str,&DoubleValue),true);
+  BOOST_REQUIRE_EQUAL(openfluid::tools::IsVeryClose(DoubleValue,0.01),true);
+  BOOST_REQUIRE_EQUAL(openfluid::tools::IsVeryClose(DoubleValue,0.02),false);
+
+  Str = "abcd";
+  BOOST_REQUIRE_EQUAL(openfluid::tools::ConvertString(Str,&BoolValue),false);
+
+  Str = "1";
+  BOOST_REQUIRE_EQUAL(openfluid::tools::ConvertString(Str,&BoolValue),true);
+  BOOST_REQUIRE_EQUAL(1,true);
+  BOOST_REQUIRE_EQUAL(0,false);
+
+  DoubleValue = 0.25;
+  BOOST_REQUIRE_EQUAL(openfluid::tools::ConvertValue(DoubleValue,&Str),true);
+  BOOST_REQUIRE_EQUAL(Str,"0.25");
+
+
+  Str = "aaa;bbbb;ccccc";
+  openfluid::tools::TokenizeString(Str,StrArray,";");
+  BOOST_REQUIRE_EQUAL(StrArray.size(),3);
+  BOOST_REQUIRE_EQUAL(StrArray[0],"aaa");
+  BOOST_REQUIRE_EQUAL(StrArray[1],"bbbb");
+  BOOST_REQUIRE_EQUAL(StrArray[2],"ccccc");
+  openfluid::tools::TokenizeString(Str,StrArray,"-");
+  BOOST_REQUIRE_EQUAL(StrArray.size(),1);
+
+  BOOST_REQUIRE_EQUAL(openfluid::tools::WildcardMatching("*","foobar"),true);
+  BOOST_REQUIRE_EQUAL(openfluid::tools::WildcardMatching("*foobar","foobar"),true);
+  BOOST_REQUIRE_EQUAL(openfluid::tools::WildcardMatching("?foobar","foobar"),false);
+  BOOST_REQUIRE_EQUAL(openfluid::tools::WildcardMatching("*bar","foobar"),true);
+  BOOST_REQUIRE_EQUAL(openfluid::tools::WildcardMatching("foo*","foobar"),true);
+  BOOST_REQUIRE_EQUAL(openfluid::tools::WildcardMatching("foo*","fobar"),false);
+  BOOST_REQUIRE_EQUAL(openfluid::tools::WildcardMatching("foo?","foobar"),false);
+  BOOST_REQUIRE_EQUAL(openfluid::tools::WildcardMatching("foo???","foobar"),true);
+  BOOST_REQUIRE_EQUAL(openfluid::tools::WildcardMatching("foo*foo","foobarfoo"),true);
+  BOOST_REQUIRE_EQUAL(openfluid::tools::WildcardMatching("foo*foo","foofoo"),true);
+  BOOST_REQUIRE_EQUAL(openfluid::tools::WildcardMatching("foo*foo","foobarfo"),false);
+  BOOST_REQUIRE_EQUAL(openfluid::tools::WildcardMatching("foo?foo","foobarfoo"),false);
+  BOOST_REQUIRE_EQUAL(openfluid::tools::WildcardMatching("foo???foo","foobarfoo"),true);
+
 }
 
 // =====================================================================
