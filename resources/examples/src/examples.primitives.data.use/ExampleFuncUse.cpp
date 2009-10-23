b9f1fe14d8c75a3b531c7a0f59466e69d3770f58
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2009-10-23 06:43:44 +0000

    * Added examples of functions, with source code, in resources * Added tests for examples functions * Removed examples directory from src tree

---
+++ resources/examples/src/examples.primitives.data.use/ExampleFuncUse.cpp
@@ -0,0 +1,153 @@
+/**
+  \file ExampleFuncProd.cpp
+*/
+
+
+#include "openfluid-base.h"
+#include "openfluid-core.h"
+
+
+// =====================================================================
+// =====================================================================
+
+
+DECLARE_PLUGIN_HOOKS;
+
+
+// =====================================================================
+// =====================================================================
+
+
+BEGIN_SIGNATURE_HOOK
+  DECLARE_SIGNATURE_ID(("examples.primitives.data.use"));
+  DECLARE_SIGNATURE_NAME(("Example of data use"));
+  DECLARE_SIGNATURE_DESCRIPTION((""));
+
+  DECLARE_SIGNATURE_VERSION(("1.0"));
+  DECLARE_SIGNATURE_SDKVERSION;
+  DECLARE_SIGNATURE_STATUS(openfluid::base::EXPERIMENTAL);
+
+  DECLARE_SIGNATURE_DOMAIN(("examples"));
+  DECLARE_SIGNATURE_PROCESS((""));
+  DECLARE_SIGNATURE_METHOD((""));
+  DECLARE_SIGNATURE_AUTHORNAME(("Jean-Christophe Fabre"));
+  DECLARE_SIGNATURE_AUTHOREMAIL(("fabrejc@supagro.inra.fr"));
+
+  
+  
+  
+  
+  
+
+END_SIGNATURE_HOOK
+
+
+// =====================================================================
+// =====================================================================
+
+
+/**
+
+*/
+class ExampleFunctionUse : public openfluid::base::PluggableFunction
+{
+  private:
+
+  
+  public:
+
+  
+    ExampleFunctionUse() : PluggableFunction()
+    {
+  
+  
+    }
+  
+  
+    // =====================================================================
+    // =====================================================================
+  
+  
+    ~ExampleFunctionUse()
+    {
+  
+  
+    }
+  
+  
+    // =====================================================================
+    // =====================================================================
+  
+  
+    bool initParams(openfluid::core::FuncParamsMap_t Params)
+    {
+  
+  
+      return true;
+    }
+  
+    // =====================================================================
+    // =====================================================================
+  
+  
+    bool prepareData()
+    {
+  
+  
+      return true;
+    }
+  
+  
+    // =====================================================================
+    // =====================================================================
+  
+  
+    bool checkConsistency()
+    {
+  
+  
+      return true;
+    }
+  
+  
+    // =====================================================================
+    // =====================================================================
+  
+  
+    bool initializeRun(const openfluid::base::SimulationInfo* SimInfo)
+    {
+  
+  
+      return true;
+    }
+  
+    // =====================================================================
+    // =====================================================================
+  
+  
+    bool runStep(const openfluid::base::SimulationStatus* SimStatus)
+    {
+  
+      return true;
+    }
+  
+    // =====================================================================
+    // =====================================================================
+  
+  
+    bool finalizeRun(const openfluid::base::SimulationInfo* SimInfo)
+    {
+  
+  
+      return true;
+    }
+
+};
+
+
+// =====================================================================
+// =====================================================================
+
+
+DEFINE_FUNCTION_HOOK(ExampleFunctionUse);
+
