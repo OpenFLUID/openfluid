6d3a08ab51f1c6a8ec6c1d025f338c8929b8eb56
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2010-03-18 13:44:22 +0000

    * Replaced TinyXML library by libXML2

--- src/ofelib/tools/DataSrcFile.cpp
+++ src/ofelib/tools/DataSrcFile.cpp
@@ -18,8 +18,11 @@
 */
 
 
+#include <libxml/parser.h>
+
 #include "DataSrcFile.h"
-#include "tinyxml.h"
+#include "SwissTools.h"
+
 
 
 namespace openfluid { namespace tools {
@@ -50,40 +53,59 @@ DataSourcesFile::~DataSourcesFile()
 bool DataSourcesFile::load(std::string Filename)
 {
 
-  TiXmlDocument LoadDoc;
-  int ID;
+  xmlDocPtr Doc = NULL;
+  xmlNodePtr Root = NULL;
+  xmlNodePtr CurrNode = NULL;
+  xmlNodePtr CurrNode2 = NULL;
 
 
   m_Loaded = true;
   m_IDs.clear();
   m_Sources.clear();
 
+  Doc = xmlParseFile(Filename.c_str());
 
-  if (LoadDoc.LoadFile(Filename.c_str()))
+  if (Doc != NULL)
   {
+    Root =  xmlDocGetRootElement(Doc);
 
-    TiXmlHandle DocHandle(&LoadDoc);
-
-    TiXmlElement* Child;
-
-
-    for(Child = DocHandle.FirstChild("openfluid").FirstChild("datasources").FirstChild("filesource").Element(); Child != 0; Child=Child->NextSiblingElement())
+    if (Root != NULL)
     {
-      if (Child->Attribute("ID",&ID) != NULL && Child->Attribute("file") != NULL)
+      if (xmlStrcmp(Root->name,(const xmlChar*)"openfluid") == 0)
       {
-        m_Sources[ID] = Child->Attribute("file");
-        m_IDs.push_back(ID);
+        CurrNode = Root->xmlChildrenNode;
+        while (CurrNode != NULL)
+        {
+          if (xmlStrcmp(CurrNode->name,(const xmlChar*)"datasources") == 0)
+          {
+            CurrNode2 = CurrNode->xmlChildrenNode;
+            while (CurrNode2 != NULL)
+            {
+
+              if (xmlStrcmp(CurrNode2->name,(const xmlChar*)"filesource") == 0)
+              {
+                xmlChar* xmlID= xmlGetProp(CurrNode2,(const xmlChar*)"ID");
+                xmlChar* xmlFile= xmlGetProp(CurrNode2,(const xmlChar*)"file");
+                int ID;
+
+                if (xmlID != NULL && xmlFile != NULL && openfluid::tools::ConvertString(std::string((char*)xmlID),&ID))
+                {
+                  m_Sources[ID] = std::string((char*)xmlFile);
+                  m_IDs.push_back(ID);
+                }
+
+              }
+              CurrNode2 = CurrNode2->next;
+            }
+          }
+          CurrNode = CurrNode->next;
+        }
       }
-      else
-      {
-        m_Loaded = false;
-      }
+      else m_Loaded = false;
     }
+    else m_Loaded = false;
   }
-  else
-  {
-    m_Loaded = false;
-  }
+  else m_Loaded = false;
 
   if (!m_Loaded)
   {
