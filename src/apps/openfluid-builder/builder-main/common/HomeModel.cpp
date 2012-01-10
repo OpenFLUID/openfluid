5ee762d3e0e32060d3501897caa9f9ac6dc43c35
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2012-01-10 10:53:44 +0000

    * Removed warning text from OpenFLUID-Builder home screen * Updated translation texts

--- src/apps/openfluid-builder/builder-main/common/HomeModel.cpp
+++ src/apps/openfluid-builder/builder-main/common/HomeModel.cpp
@@ -65,10 +65,7 @@
 HomeModelImpl::HomeModelImpl()
 {
   m_OFVersionTxt
-      = "OpenFLUID v" + openfluid::config::FULL_VERSION + std::string(
-          "\n\n<span color='#FBAF40' font-style='italic'>")
-          + _("This is a preview release of OpenFLUID-Builder software.\nSome features have been temporarily disabled.")
-          + std::string("</span>");
+      = "OpenFLUID v" + openfluid::config::FULL_VERSION;
   m_OFWebSite = "http://www.umr-lisah.fr/openfluid/";
 }
 
