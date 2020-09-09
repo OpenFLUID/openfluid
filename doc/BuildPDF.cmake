2e6b803eb2edf7bb1bf44fc24784922003e3b586
Jean-Christophe Fabre <jean-christophe.fabre@inrae.fr>
2020-09-09 12:03:51 +0200

    Update of doc building process for PDF
    * Redirect verbose build output to files
    * Refactored build process script
    * Fixed doxygen configuration
    * Switched to version 2.1.11
    
    (closes OpenFLUID/openfluid#972)

--- doc/BuildPDF.cmake
+++ doc/BuildPDF.cmake
@@ -3,19 +3,11 @@
 # PDFLATEX_COMPILER
 
 
+# Builds the pdf doc in 3 passes
+FOREACH(PASS RANGE 1 3)
     EXECUTE_PROCESS(
-      COMMAND ${CMAKE_COMMAND} -E chdir ${DOC_BUILD_DIR}/latex ${PDFLATEX_COMPILER}
-              "-interaction=nonstopmode" "main.tex"
+      COMMAND ${CMAKE_COMMAND} -E chdir "${DOC_BUILD_DIR}/latex"
+              ${PDFLATEX_COMPILER} "-interaction=nonstopmode" "main.tex"
+      OUTPUT_FILE "${DOC_BUILD_DIR}/latex/buildpass${PASS}_out.txt"
     )
-    
-    EXECUTE_PROCESS(       
-      COMMAND ${CMAKE_COMMAND} -E chdir ${DOC_BUILD_DIR}/latex ${PDFLATEX_COMPILER}
-              "-interaction=nonstopmode" "main.tex"
-    )
-    
-    EXECUTE_PROCESS(
-      COMMAND ${CMAKE_COMMAND} -E chdir ${DOC_BUILD_DIR}/latex ${PDFLATEX_COMPILER}
-              "-interaction=nonstopmode" "main.tex"              
-    )
-    
-    
+ENDFOREACH()
