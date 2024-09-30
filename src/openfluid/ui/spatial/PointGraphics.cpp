14308278128350b56fb29414043732f17f17b3e7
Dorian Gerardin <dorian.gerardin@gmail.com>
2024-09-30 09:56:14 +0200

    Exposition of maps graphics classes to api in new ui/spatial namespace

---
+++ src/openfluid/ui/spatial/PointGraphics.cpp
@@ -0,0 +1,60 @@
+/*
+
+  This file is part of OpenFLUID software
+  Copyright(c) 2007, INRA - Montpellier SupAgro
+
+
+ == GNU General Public License Usage ==
+
+  OpenFLUID is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OpenFLUID is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OpenFLUID. If not, see <http://www.gnu.org/licenses/>.
+
+
+ == Other Usage ==
+
+  Other Usage means a use of OpenFLUID that is inconsistent with the GPL
+  license, and requires a written agreement between You and INRA.
+  Licensees for Other Usage of OpenFLUID may use this file in accordance
+  with the terms contained in the written agreement between You and INRA.
+  
+*/
+
+
+/**
+  @file PointGraphics.cpp
+
+  @author Jean-Christophe FABRE <jean-christophe.fabre@inra.fr>
+*/
+
+
+#include <QPen>
+
+#include "PointGraphics.hpp"
+
+
+namespace openfluid { namespace ui { namespace spatial {
+
+
+PointGraphics::PointGraphics(OGRPoint* OGRPt, const QPen& Pen):
+  PunctualGraphics(Pen.color())
+{
+  QPainterPath Path;
+
+  setPen(Pen);
+
+  drawPoint(Path,OGRPt);
+
+  setPath(Path);
+}
+
+} } } // namespaces
