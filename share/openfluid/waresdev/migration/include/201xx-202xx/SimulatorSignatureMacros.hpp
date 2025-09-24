7033a2423ad81c6843f39c398072cf65c54c1342
Armel Thöni <armel.thoni@inrae.fr>
2025-09-24 16:15:22 +0200

    Minor fix about empty tags during signature migration

--- share/openfluid/waresdev/migration/include/201xx-202xx/SimulatorSignatureMacros.hpp
+++ share/openfluid/waresdev/migration/include/201xx-202xx/SimulatorSignatureMacros.hpp
@@ -35,6 +35,7 @@
   @author Jean-Christophe FABRE <jean-christophe.fabre@inrae.fr>
 */
 
+// OpenFLUID:stylecheck:!brac
 
 #ifndef __OPENFLUID_SHARE_MIGRATION_SIMULATORSIGNATUREMACROS_HPP__
 #define __OPENFLUID_SHARE_MIGRATION_SIMULATORSIGNATUREMACROS_HPP__
@@ -70,19 +71,19 @@
   Macro for declaration of the simulator application domain
 */
 #define DECLARE_DOMAIN(domain) \
-  Signature->Tags.push_back("domain::" + std::string(domain));
+  if(!std::string(domain).empty()) Signature->Tags.push_back("domain::" + std::string(domain));
 
 /**
   Macro for declaration of the simulator application processes
 */
 #define DECLARE_PROCESS(process) \
-  Signature->Tags.push_back("process::" + std::string(process));
+  if(!std::string(process).empty()) Signature->Tags.push_back("process::" + std::string(process));
 
 /**
   Macro for declaration of the simulator application numerical method
 */
 #define DECLARE_METHOD(method) \
-  Signature->Tags.push_back("method::" + std::string(method));
+  if(!std::string(method).empty()) Signature->Tags.push_back("method::" + std::string(method));
 
 
 // =====================================================================
